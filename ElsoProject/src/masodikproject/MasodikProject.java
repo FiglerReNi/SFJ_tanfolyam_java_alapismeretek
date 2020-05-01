31d08cd2a44223d06177c2bb31342cdcbaa2c518
FiglerReNi <renatafigler85@gmail.com>
2020-05-01 14:09:15 +0200

    forras

--- ElsoProject/src/masodikproject/MasodikProject.java
+++ ElsoProject/src/masodikproject/MasodikProject.java
@@ -81,7 +81,67 @@ public class MasodikProject {
        double e = 3.5;
        //int f = e; //---> ezt így nem lehet, mert int nem tud double-t
        
+       //Kasztolás 
+       String szoveg = "a";  //a string objektum, vagyis vele együtt létre jön az object a java ősosztálya is
+                             //így lesznek beépített elérhető metódusai, függvényei
+       //a primitívekre nem tudok semmi ilyet meghívni, mert nem objektumok.
+       int szam = 2; // nem csak a korábbi megoldssal csinálhatunk belőle stringet
        
+       //a .valueOf() egy overloaded function (túltöltött), mert ugyanaz a nevük, de különböző fajta paramétereket várnak
+       szoveg = String.valueOf(szam); // -->igy be tudjuk tenni a stringbe az intet, de innentől szövegként tárolódik
        
+       //Minden primitivnek létezik egy becsomagoló osztálya:
+       /*   primitivek            objektumok
+            boolean                 Boolean
+            char                    Character
+            byte                    Byte
+            short                   Short
+            int                     Integer
+            long                    Long
+            float                   Float
+            double                  Double
+       */
+       
+       char teszt1 = 'c'; //ez így primitív nem tudok dolgoat meghívni rá
+       Character teszt2 = 'c'; //így viszont már objektum, meghívhatók rá dolgok, de több helyet foglal így
+                               //viszont így könnyebb bonyolultabb dolgokat csinálni velük
+       teszt2.toString();
+       
+       test(teszt1); //bár objetumot vár, meg tudom hívni a primitív párjával is ezeket, és a java automatikusan becsomagolja
+                     //és lesz belőle objektum
+       Integer pelda = 2; //fordítva is működik
+       test1(pelda);
+       
+       int x = 5;
+       int y = x; //Primitíveknél a program lemásolja x értékét, utána ők különböző életet élnek
+       
+       x++; // x=6 y még mindig 5
+        System.out.println("" + x + " " + y);
+        
+        Cat macskaTest1 = new Cat();
+        Cat macskaTest2 = macskaTest1;       
+        macskaTest2.setName("Fifi");     
+        System.out.println(macskaTest1.getName() + " " + macskaTest2.getName());
+        // mindkettő Fifi lesz, mert objetumoknál csak referenciák vannak, olyan változók ami csk rámutat,
+        //új objektum minig csak new-al jön létre ---> ha egyiken módosítok, mási is módosít
+       
+       /*Manuális kasztolás*/
+       int szamX = 2;
+       Integer szamY = szamX;
+       
+       double szamZ = 3.5;  //ezt szeretném int-be átvinni
+       int szamV = (int)szamZ; //de ebben az esetben leveszi a tizedes utáni összes számot és nem kerekít
+        System.out.println(szamV);
+       
+        Integer it = 2;
+        String something = it.toString(); //----> stringé tudom alakítani
+ 
+    }
+    public static void test(Character a) {
+        System.out.println(a);
+    }
+
+    public static void test1(int a) {
+        System.out.println(a);
     }
 }
