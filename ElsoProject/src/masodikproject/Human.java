1337102c96a822067903129640f655540423c775
FiglerReNi <50335163+FiglerReNi@users.noreply.github.com>
2021-05-16 19:17:14 +0200

    forras

--- ElsoProject/src/masodikproject/Human.java
+++ ElsoProject/src/masodikproject/Human.java
@@ -1,12 +1,9 @@
 package masodikproject;
 
 public class Human {
-    //lehet public: ha nem írunk oda semmit automatikusan az methodnál és propertynél is -> bárhol elérhető
-    //lehet private, nem érhető el kívülről -> ezt használjuk leginkább
-    //private String name = "Gyula";
     
-    private String name; //ha nem adok meg semmit null lesz
-    private int age; //ha nem adok meg értéket az alap nulla lesz
+    private String name; 
+    private int age; 
     
     public String getName(){
         return this.name;
@@ -23,14 +20,10 @@ public class Human {
     public void setAge(int age) {
         this.age = age;
     }
-    //A lenti felépítésűkomment bekerül a javadoc-ba
-    /** Ez a metódus kiírja az ember nevét */ 
+
     void writeMyName(){
-        //lokális változó lesz, vagyis itt bent ezt fogja hasznélni és kiírni
         String name = "Jani";
         System.out.println("Az én nevem: " + name);
-        //ebben az esetben a this miatt nem a lokális változót keresi, hanem az objektumét. A this mindig az objektumra
-        //mutat.
         System.out.println("Az én nevem: " + this.name);
     }  
 }
