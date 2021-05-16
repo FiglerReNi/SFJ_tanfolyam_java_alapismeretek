df9a8bf3ac64fe6c73011ea5cc974210563a1ca1
FiglerReNi <50335163+FiglerReNi@users.noreply.github.com>
2021-05-16 18:46:23 +0200

    forras

--- ElsoProject/src/masodikproject/Animal.java
+++ ElsoProject/src/masodikproject/Animal.java
@@ -1,16 +1,10 @@
 package masodikproject;
-//többszörös öröklődés nincs a javaban, egyetlen szülője lehet egy osztálynak
-//abstract --> ezt az osztályt önmagában nem lehet példányosítani
-//ha az abstract osztálynakk van egy abstract metódusa, akor azt kötelező override-olni, mint az implementációnál
+
 public abstract class Animal {
     
     private String name;
     private int weight;
    
-    //public bárhol elérhető
-    //protected csak adott packagen belül érhető el
-    //ha ezt private-ra állítom, akkor nem elérhető máshol, változóra is igaz
-    //ha protectedre állítom akkor elérhető lesz az alosztályok számára is
     protected void shout(){
         System.out.println("Kiáltás");
     }
