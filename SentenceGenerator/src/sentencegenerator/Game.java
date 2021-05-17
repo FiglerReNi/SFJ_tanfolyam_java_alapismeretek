73c97c098a659c41aa0fbb38b96e7067dc685ae0
FiglerReNi <50335163+FiglerReNi@users.noreply.github.com>
2021-05-17 14:25:45 +0200

    forras

--- SentenceGenerator/src/sentencegenerator/Game.java
+++ SentenceGenerator/src/sentencegenerator/Game.java
@@ -11,9 +11,9 @@ public class Game {
 //        int secondLength = secondWord.length;
 //        int thirdLength = thirdWord.length;
 //
-//        int random1 = (int) (Math.random() * oneLength);        //0 és 1 között ad vissza törtet alapesetben
-//        int random2 = (int) (Math.random() * secondLength);    //ha kasztolunk alapból mindig nullát kapunk    
-//        int random3 = (int) (Math.random() * thirdLength);    //ezért * --> 0-4 között kapunk ha 5-el szorzunk
+//        int random1 = (int) (Math.random() * oneLength);       
+//        int random2 = (int) (Math.random() * secondLength);   
+//        int random3 = (int) (Math.random() * thirdLength);   
 
         int random1 = (int) (Math.random() * firstWord.length);        
         int random2 = (int) (Math.random() * secondWord.length);   
